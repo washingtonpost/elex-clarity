029de54db2eaf4a8fecc2b837753811432766b07
Anthony Pesce <anthonyjpesce@gmail.com>
2020-11-12 13:44:24 -0500

    savepoint

--- src/elexclarity/convert.py
+++ src/elexclarity/convert.py
@@ -1,7 +1,25 @@
-def convert(data, *, outputType="results", level=None, style="default", resultsBy=None, **kwargs):
+import xmltodict
+from slugify import slugify
+from collections import defaultdict
+from elexstatic import STATES
+
+
+class ClarityXMLConverter:
     """
-    The entry point for formatting Clarity results data.
+    A class to convert Clarity XML into our expected data format.
     """
-    if style == "raw":
-        return data
-    raise Exception("No Clarity formatters implemented yet")
+    def __init__(self, XML_data, **kwargs):
+        self.data = xmltodict.parse(data, attr_prefix="")["ElectionResult"]
+        self.county_name = 
+
+
+
+
+
+# def convert(data, *, outputType="results", level=None, style="default", resultsBy=None, **kwargs):
+#     """
+#     The entry point for formatting Clarity results data.
+#     """
+#     if style == "raw":
+#         return data
+#     raise Exception("No Clarity formatters implemented yet")
