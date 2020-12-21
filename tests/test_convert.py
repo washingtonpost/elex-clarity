2385169e953ed01f8af94c61473b692dba3ae1b4
Peter Andringa <peter.andringa@washpost.com>
2020-12-21 17:51:40 -0500

    test: add tests for single result and officeid filtering

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -23,3 +23,15 @@ def test_county_level_results(ga_counties, ga_county_mapping_fips):
 
     assert len(results) == 2
     assert "2020-11-03_GA_G_P" in results
+
+def test_single_result_object(ga_counties, ga_county_mapping_fips):
+    results = convert(ga_counties, statepostal="GA", level="county", countyMapping=ga_county_mapping_fips)
+
+    assert len(results) == 2
+    assert "2020-11-03_GA_G_P" in results
+
+def test_filter_officeid(ga_counties, ga_county_mapping_fips):
+    results = convert(ga_counties, statepostal="GA", level="county", countyMapping=ga_county_mapping_fips, officeID="P")
+
+    assert len(results) == 1
+    assert "2020-11-03_GA_G_P" in results
