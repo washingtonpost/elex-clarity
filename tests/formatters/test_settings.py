5e0e519511604b0e4302231750d96758cb373dab
Peter Andringa <peter.andringa@washpost.com>
2020-12-22 10:10:32 -0500

    test: update test case for settings formatter

--- tests/formatters/test_settings.py
+++ tests/formatters/test_settings.py
@@ -8,12 +8,14 @@ def test_format_county_settings(recorder, api_client, ga_county_mapping_fips):
         assert len(result) == 159
         assert result["2020-11-03_GA_G_S_13001"] == {
             'clarityId': '105371',
+            'name': 'Appling',
             'id': '2020-11-03_GA_G_S_13001',
             'lastUpdated': '2020-11-16T20:48:35Z',
             'version': '271560'
         }
         assert result["2020-11-03_GA_G_S_13277"] == {
             'clarityId': '105507',
+            'name': 'Tift',
             'id': '2020-11-03_GA_G_S_13277',
             'lastUpdated': '2020-11-06T22:50:20Z',
             'version': '270028'
