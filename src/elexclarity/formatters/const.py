a734333f70866abbe409764f5e2da1f20c35d7e7
Chris Zubak-Skees <chriszs@gmail.com>
2022-05-24 09:27:31 -0400

    feat: add additional race type partisan mappings

--- src/elexclarity/formatters/const.py
+++ src/elexclarity/formatters/const.py
@@ -20,10 +20,16 @@ STATE_OFFICE_ID_MAPS = {
 STATE_RACE_TYPE_MAPS = {
     "GA": {
         'US Senate - Rep': 'R',
+        'REP - US Senate': 'R',
+        'DEM - US Senate': 'D',
         'US Senate - Dem': 'D',
         'Governor - Rep': 'R',
+        'REP - Governor': 'R',
+        'DEM - Governor': 'D',
         'Governor - Dem': 'D',
         'Secretary of State - Rep': 'R',
+        'REP - Secretary of State': 'R',
+        'DEM - Secretary of State': 'D',
         'Secretary of State - Dem': 'D',
         'Primary': 'P'
     }
